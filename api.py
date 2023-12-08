be98c4eef8e3e8a1dff2c3b05463faaca6d12f6e
Srinivasa Tadipatri <reddyfull@gmail.com>
2023-12-08 09:26:07 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,27 +1,38 @@
+import os
 import json
 import requests
 
-def read_json_from_file(file_path):
-    with open(file_path, 'r') as file:
-        return json.load(file)
-
-def get_api_config_from_env(env_var_name):
-    api_config_json = os.getenv(env_var_name)
+def get_api_config():
+    api_config_json = os.getenv("API_CONFIG_JSON")
     if not api_config_json:
-        raise ValueError(f"{env_var_name} not found in environment variables")
+        raise ValueError("API configuration not found in environment variables")
     return json.loads(api_config_json)
 
-def make_api_call(api_details, credentials):
-    # ... your logic for making API calls ...
+def make_api_request(api_details):
+    url = api_details["url"]
+    method = api_details["method"]
+    headers = api_details.get("headers", {})
+    body = api_details.get("body", {})
+
+    if method.upper() == "GET":
+        response = requests.get(url, headers=headers)
+    elif method.upper() == "POST":
+        response = requests.post(url, headers=headers, json=body)
+    else:
+        raise ValueError(f"Unsupported HTTP method: {method}")
+
+    return response
 
 def main():
-    credentials = read_json_from_file('credentials.json')
-    api_config = get_api_config_from_env('API_CONFIG_JSON')
+    api_config = get_api_config()
 
-    for api_key, api_details in api_config.items():
+    for api_name, api_details in api_config.items():
         if api_details.get("status", "").lower() == "yes":
-            response = make_api_call(api_details, credentials)
-            # Handle the response
+            response = make_api_request(api_details)
+            print(f"Response from {api_name}: {response.status_code}")
+            print(f"Response body: {response.json()}")
+        else:
+            print(f"Skipping {api_name} as its status is not 'yes'.")
 
 if __name__ == "__main__":
     main()
