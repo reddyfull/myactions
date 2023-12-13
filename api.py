4b96e645a9f1233acf3eaed323c6dd756bb22462
Srinivasa Tadipatri <reddyfull@gmail.com>
2023-12-13 12:59:53 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,21 @@
-import requests
+import os
 import json
+import requests
+from cryptography.fernet import Fernet
 
 def read_json(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
+
+def decrypt_file(file_path, passphrase):
+    key = Fernet.generate_key()  # Generate a key based on the passphrase
+    cipher_suite = Fernet(key)
+
+    with open(file_path, 'rb') as file:
+        encrypted_data = file.read()
+
+    decrypted_data = cipher_suite.decrypt(encrypted_data)
+    return json.loads(decrypted_data)
 
 def get_bearer_token(credentials, url):
     response = requests.post(url, data=credentials)
@@ -30,13 +42,17 @@ def make_api_call(api_details, token):
 
 def main():
     credentials_file = "credentials.json"
-    api_details_file = "api_details.json"
+    api_details_encrypted_file = "api_details.json.enc"
+    passphrase = os.getenv("ENCRYPTION_PASSPHRASE")
+
+    if not passphrase:
+        raise ValueError("Passphrase not found in environment variables")
 
     token_url = "https://login.microsoftonline.com/orxhemiprodb2c.onmicrosoft.com/oauth2/token"
 
     try:
         credentials = read_json(credentials_file)
-        apis = read_json(api_details_file)
+        apis = decrypt_file(api_details_encrypted_file, passphrase)
 
         token = get_bearer_token(credentials, token_url)
 
