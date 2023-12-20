7e16f9755ac1365b0f4997b052e7f2cfdc51cfd5
Srinivasa Tadipatri <reddyfull@gmail.com>
2023-12-20 11:09:17 -0500

    Update your_script.py

--- .github/scripts/your_script.py
+++ .github/scripts/your_script.py
@@ -1,23 +1,35 @@
 import os
 import requests
+import json
+
+def get_repo_from_url(url):
+    parts = url.split('/')
+    user, repo = parts[3], parts[4]
+    return user, repo
+
+def get_tags(token, user, repo):
+    headers = {"Authorization": f"token {token}"}
+    url = f"https://api.github.com/repos/{user}/{repo}/tags"
+    response = requests.get(url, headers=headers)
+    if response.ok:
+        return response.json()
+    else:
+        raise Exception(f"Failed to fetch tags: {response.content}")
 
 def main():
     token = os.getenv('GITHUB_TOKEN')
     if not token:
         raise Exception("GitHub token not found")
 
-    headers = {"Authorization": f"token {token}"}
-    response = requests.get('https://api.github.com/user', headers=headers)
+    user, repo = get_repo_from_url("https://github.com/reddyfull/myactions")
+    tags = get_tags(token, user, repo)
 
-    if response.ok:
-        user_data = response.json()
-        print("User Login: ", user_data.get('login'))
-        print("User ID: ", user_data.get('id'))
-        print("User Name: ", user_data.get('name'))
-        print("Number of Public Repos: ", user_data.get('public_repos'))
-        # You can add more fields to print as needed
-    else:
-        print("Failed to fetch user data")
+    tags_data = [{"name": tag["name"], "commit": tag["commit"]["sha"]} for tag in tags]
+
+    file_path = ".github/data/tags.json"
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "w") as file:
+        json.dump(tags_data, file, indent=4)
 
 if __name__ == "__main__":
     main()
