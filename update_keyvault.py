af7615b3cc6dfeab51d635a5332e19ca688e612c
reddyfull <reddyfull@gmail.com>
2024-01-04 11:48:04 -0500

    keyvault

--- update_keyvault.py
+++ update_keyvault.py
@@ -23,17 +23,27 @@ with open('keyvault.json') as file:
 # Insert secrets as per JSON instructions
 for secret_name, secret_value in secrets_data.get('insert', {}).items():
     try:
-        client.set_secret(secret_name, secret_value)
-        print(f"Secret {secret_name} set successfully.")
+        existing_secret = None
+        try:
+            existing_secret = client.get_secret(secret_name)
+        except ResourceNotFoundError:
+            pass  # Secret doesn't exist, which is fine
+        
+        if not existing_secret or existing_secret.value != secret_value:
+            client.set_secret(secret_name, secret_value)
+            print(f"Secret '{secret_name}' set successfully.")
+        else:
+            print(f"Secret '{secret_name}' already exists with the same value. No action taken.")
+
     except HttpResponseError as e:
-        print(f"Failed to set secret {secret_name}. Error: {e}")
+        print(f"Failed to set secret '{secret_name}'. Error: {e}")
 
 # Delete secrets as per JSON instructions, if any
 for secret_name in secrets_data.get('delete', []):
     try:
         client.begin_delete_secret(secret_name).result()
-        print(f"Secret {secret_name} deleted successfully.")
+        print(f"Secret '{secret_name}' deleted successfully.")
     except ResourceNotFoundError:
-        print(f"Secret {secret_name} not found. No action taken.")
+        print(f"Secret '{secret_name}' not found. No action taken.")
     except HttpResponseError as e:
-        print(f"Failed to delete secret {secret_name}. Error: {e}")
+        print(f"Failed to delete secret '{secret_name}'. Error: {e}")
