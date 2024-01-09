bfedb1f2b868aa84ba2a249244e1fa21ef731f48
reddyfull <reddyfull@gmail.com>
2024-01-09 11:13:49 -0500

    appconfigs

--- push_config_to_app_config.py
+++ push_config_to_app_config.py
@@ -1,9 +1,12 @@
-from azure.appconfiguration import AzureAppConfigurationClient
+from azure.appconfiguration import AzureAppConfigurationClient, ConfigurationSetting
 import os
 import json
 
 # Retrieve connection string from environment variable
 connection_string = os.getenv('APP_CONFIG_CONNECTION')
+
+if not connection_string:
+    raise ValueError("Connection string not found in environment variables. Please check your GitHub secrets configuration.")
 
 config_client = AzureAppConfigurationClient.from_connection_string(connection_string)
 
@@ -13,6 +16,7 @@ with open('config.json') as config_file:
 
 # Push configurations to Azure App Configuration
 for key, value in config_data.items():
-    config_client.set_configuration_setting(key=key, value=value)
+    setting = ConfigurationSetting(key=key, value=value)
+    config_client.set_configuration_setting(setting)
 
 print("Configurations pushed to Azure App Configuration.")
